b2600f3baaf4c7f6361133c80b8140c4cef8411c
olharubel <88097930+olharubel@users.noreply.github.com>
2021-10-26 21:23:25 +0300

    Add files via upload

---
+++ Task9/Task9/Product.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task9
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        private double weight;
+        private int expirationDate;
+        private DateTime creationDate;
+
+        public Product(string name, double price, double weight, int expirationDate, DateTime creationDate)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+            this.expirationDate = expirationDate;
+            this.creationDate = creationDate;
+        }
+
+        public Product(string name, double price, double weight)
+        {
+            Name = name;
+            Price = price;
+            Weight = weight;
+        }
+
+
+
+        public Product()
+        {
+            Name = "";
+            Price = 1;
+            Weight = 1;
+            expirationDate = 1;
+            creationDate = DateTime.Now;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public int ExpirationDate
+        {
+            get
+            {
+                return expirationDate;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    expirationDate = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect value of expiration date");
+                }
+            }
+        }
+
+        public DateTime CreationDate
+        {
+            get
+            {
+                return creationDate;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    price = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect value of price");
+                }
+            }
+        }
+
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                if (value > 0.0)
+                {
+                    weight = value;
+                }
+                else
+                {
+                    throw new ArgumentException("Incorrect value of weight");
+                }
+            }
+        }
+
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj.GetType() != this.GetType())
+                return false;
+
+            var other = (Product)obj;
+            return (Name == other.Name && Price == other.Price && Weight == other.Weight
+                && ExpirationDate == other.ExpirationDate && CreationDate == other.CreationDate);
+        }
+
+        public override String ToString()
+        {
+            return $"Name: {name}, price = {price}, weight = {weight}, expDate = {expirationDate}, " +
+                $"{creationDate:d.MM.yyyy}";
+        }
+
+        public virtual void ChangePrice(int percent)
+        {
+            if (percent < -100 || percent > 100)
+            {
+                throw new ArgumentException("Incorrect value of percent");
+            }
+            Price += Price * (percent / 100.0);
+        }
+
+        public virtual void Parse(string data)
+        {
+
+            string[] info = data.Split();
+
+            if (info.Length != 5)
+            {
+                throw new ArgumentException("Incorrect format of data");
+            }
+
+            Name = info[0];
+            IFormatProvider formatter = new NumberFormatInfo { NumberDecimalSeparator = "." };
+            Price = Convert.ToDouble(info[1], formatter);
+            Weight = Convert.ToDouble(info[2], formatter);
+            ExpirationDate = Convert.ToInt32(info[3]);
+            string[] date = info[4].Split('.');
+            int day = Convert.ToInt32(date[0]);
+            int month = Convert.ToInt32(date[1]);
+            int year = Convert.ToInt32(date[2]);
+            creationDate = Convert.ToDateTime(new DateTime(year, month, day));
+        }
+    }
+}
---
+++ Task9/Task9/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Task9
+{
+    class Program
+    {
+        static void WriteWrongItemIntoFile(string line)
+        {
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Ростик\source\repos\Task9\Task9\WrongProductsInput.txt", true))
+            {
+                sw.WriteLine(DateTime.Now);
+                sw.WriteLine("Incorrect product");
+                sw.Write(line + " ");
+            }
+        }
+
+        static void CorrectWrongData(Storage storage, string line)
+        {
+            Console.WriteLine("Incorrect product");
+            Console.WriteLine(line);
+            Console.WriteLine("Do you want to correct wrong format?");
+            char answer = (char)Console.Read();
+            Console.ReadLine();
+            switch (answer)
+            {
+                case 'y':
+                    {
+                        ConsoleStorageManager storageManager = new ConsoleStorageManager();
+                        storageManager.EnterCorrectProductData(storage);
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+        }
+
+        static void RemoveExpiredProducts(Storage storage)
+        {
+            var expiredProducts = storage.FindExpiredProducts();
+            using (StreamWriter sw = new StreamWriter(@"C:\Users\Ростик\source\repos\Task9\Task9\RemovedExpiredProducts.txt", true))
+            {
+                sw.WriteLine("Expired product");
+                foreach (var p in expiredProducts)
+                {
+                    sw.WriteLine(p.ToString());
+                }
+            }
+
+            storage.RemoveExpiredProducts();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Storage s = new Storage();
+            ConsoleStorageManager storageManager = new ConsoleStorageManager();
+            try
+            {
+                s.OnWriteWrongInput += WriteWrongItemIntoFile;
+                s.OnCorrectInput += CorrectWrongData;
+                storageManager.OnFind += RemoveExpiredProducts;
+                s.ReadFromFile(@"C:\Users\Ростик\source\repos\Task9\Task9\ListOfProducts.txt");
+                Console.WriteLine("Storage:");
+                storageManager.PrintAllProducts(s);
+
+                var products = s.FindProductByAtribute(Attributes.CreationDate, new DateTime(2021, 08, 20));
+                Console.WriteLine("----------------------------------");
+                foreach (var p in products)
+                {
+                    Console.WriteLine(p.ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}

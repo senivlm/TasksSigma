4e5b38c95fa453af6940a625cef1e0c9e98d32fb
olharubel <88097930+olharubel@users.noreply.github.com>
2021-10-26 21:34:51 +0300

    Add files via upload

---
+++ Task8/Task8_3/Task8_3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task8_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TextWorker t = new TextWorker();
+                t.ReadFromFile(@"C:\Users\Ростик\source\repos\Task8_3\Task8_3\Text.txt");
+                Console.WriteLine(t.ToString());
+                Console.WriteLine("Sentence with max depth");
+                Console.WriteLine(t.GetMostDepthSentence());
+                Console.WriteLine();
+                Console.WriteLine(t.ToString());
+                t.SortSentences();
+                Console.WriteLine();
+                Console.WriteLine(t.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Task8/Task8_3/Task8_3/TextWorker.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Task8_3
+{
+    class TextWorker
+    {
+        List<string> sentences;
+        Dictionary<int, string> linesByIndex;
+
+        public TextWorker()
+        {
+            linesByIndex = new Dictionary<int, string>();
+            sentences = new List<string>();
+        }
+
+        public void ReadFromFile(string path)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    StringBuilder sb = new StringBuilder();
+                    string data = "";
+                    bool isEmpty = true;
+                    int count = 0;
+                    int i = 0;
+
+                    while (!sr.EndOfStream)
+                    {
+
+                        data = sr.ReadLine();
+                        i = 0;
+
+                        while (i < data.Length)
+                        {
+
+                            while (i < data.Length && data[i] != '.')
+                            {
+                                if (data[i] == '<' || data[i] == '>')
+                                {
+                                    count++;
+                                }
+                                i++;
+                            }
+
+                            sb.Append(data);
+
+                            if (!data.Contains('.'))
+                            {
+                                continue;
+                            }
+
+                            if (data[i] == '.')
+                            {
+                                if (!linesByIndex.ContainsKey(count))
+                                {
+                                    linesByIndex.Add(count, sb.ToString());
+                                }
+                                else
+                                {
+                                    linesByIndex[count] += sb.ToString();
+                                }
+                                sentences.Add(sb.ToString());
+
+                                sb.Clear();
+                                count = 0;
+                                break;
+                            }
+                        }
+                        isEmpty = false;
+                    }
+                    if (isEmpty)
+                    {
+                        throw new Exception("File is empty!");
+                    }
+
+                }
+            }
+            catch (Exception exc)
+            {
+                throw exc;
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var elem in sentences)
+            {
+                sb.Append(elem.ToString() + "\n");
+            }
+            return sb.ToString();
+        }
+
+
+        public string GetMostDepthSentence()
+        {
+            int maxDepth = linesByIndex.Keys.Max();
+            string sentence = linesByIndex[maxDepth];
+            return sentence;
+        }
+
+
+        public void SortSentences()
+        {
+            sentences.Sort((s1, s2) => s1.Length.CompareTo(s2.Length));
+        }
+    }
+}
